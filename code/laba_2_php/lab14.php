b37ebdec5660f89b5d4b5d8fa15d8792f6d40f92
MeowAntony <antonsmirnov2233@gmail.com>
2022-02-12 18:16:22 +0200

    laba_2_php_14_2

--- code/laba_2_php/lab14.php
+++ code/laba_2_php/lab14.php
@@ -10,8 +10,31 @@ echo nl2br($a % $b . "\n");
 $a = 2000;
 $b = 16;
 if ($a % $b === 0) {
-    echo "Делится. Результат: " . ($a / $b);
+    echo nl2br("Делится. Результат: " . ($a / $b) . "\n");
 }
 else {
-    echo "Делится с остатком. Остаток: " . ($a % $b);
+    echo nl2br("Делится с остатком. Остаток: " . ($a % $b) . "\n");
 }
+
+
+$st = pow(2, 10);
+echo nl2br("2^10 = " . $st . "\n");
+
+echo nl2br("sqrt(245) = " . sqrt(245) . "\n");
+
+$array = [
+    4, #16 4 25 361 169 0 100
+    2, 
+    5,
+    19,
+    13,
+    0,
+    10
+];
+$sum_numbers = 0;
+foreach ($array as $value) {
+    $sum_numbers += $value ** 2;
+}
+echo nl2br("answer = " . sqrt($sum_numbers));
+
+
