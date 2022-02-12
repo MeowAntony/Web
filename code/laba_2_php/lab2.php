26c42c7a6b82d744ccdef331ccfe21879c83a988
MeowAntony <antonsmirnov2233@gmail.com>
2022-02-12 17:42:29 +0200

    laba_2_php_2.5

--- code/laba_2_php/lab2.php
+++ code/laba_2_php/lab2.php
@@ -13,3 +13,10 @@ echo nl2br("$myNewNumber");
 echo nl2br("\n");
 
 echo nl2br($myNumber + 2);
+
+echo nl2br("\n");
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+
+echo nl2br($last_month - $this_month);
