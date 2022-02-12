fd54266dee0a93a943e0a61fd2e6ca4685665ddd
MeowAntony <antonsmirnov2233@gmail.com>
2022-02-12 20:24:17 +0200

    laba_2_php_17.2

--- code/laba_2_php/lab17.php
+++ code/laba_2_php/lab17.php
@@ -46,3 +46,44 @@ for ($i = 0; $i < 3; $i++) {
         $index_now += 1;
     }
 }
+
+$mas = [
+    2,
+    5,
+    3,
+    9
+];
+$result = $mas[0] * $mas[1] + $mas[2] * $mas[3];
+echo nl2br("result = $result\n");
+
+$mas = [
+    "name" => "Антон",
+    "surname" => "Смирнов",
+    "patronymic" => "Сергеевич"
+];
+echo nl2br("ФИО: " . $mas["surname"] . ' ' . $mas["name"] . ' ' . $mas["patronymic"] . "\n");
+
+$date = [
+    "year" => 2022,
+    "month" => 2,
+    "day" => 12
+];
+echo nl2br($date["year"] . "-" . $date["month"] . "-" . $date["day"] . "\n");
+
+$arr = ['a', 'b', 'c', 'd', 'e'];
+echo nl2br("Count: " . sizeof($arr) . "\n");
+#либо
+$count = 0;
+foreach ($arr as $value) {
+    $count += 1;
+}
+echo nl2br("Count: $count\n");
+
+$arr = ['a', 'b', 'c', 'd', 'e'];
+# находим количество элементов
+$count = 0;
+foreach ($arr as $value) {
+    $count += 1;
+}
+echo nl2br("Last: " . $arr[$count - 1] . "\n");
+echo nl2br("PreLast: " . $arr[$count - 2] . "\n");
